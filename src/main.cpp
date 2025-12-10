013dc421e9668abc6eb3c619082d9c009fcc1f34
piotr <pioterk906@gmail.com>
2025-12-10 23:19:05 +0100

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-   std::cout << "$ ";
-   std::string buff;
-   std::cin >> buff;
-   std::cerr << buff << ": command not found" << std::endl;
-
+  std::string buff;
+  while (1)
+  {
+    std::cout << "$ ";
+    std::cin >> buff;
+    std::cerr << buff << ": command not found" << std::endl;
+  }
 }
